491531fe2f3cd8d98d88962811579dc74481c623
RYOJI <baleonly@gmail.com>
2018-04-17 21:33:08 +0900

    update post index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+@user = User.create!(email: "test@gmail.com", password: "hogehoge", password_confirmation: "hogehoge", first_name: "jon", last_name: "dan")
+
+puts "1 user created"
+
 100.times do |post|
-	Post.create(date: Date.today, rationale: "#{post} rationale content")
+	Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
