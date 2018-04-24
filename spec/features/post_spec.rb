d233a7fa5a1c7c194863f4efd78c0f75cf12f991
RYOJI <baleonly@gmail.com>
2018-04-24 16:49:22 +0900

    add scope for ristricting seeing from other user to post index

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -10,18 +10,34 @@ feature 'navigate' do
     background do
       visit posts_path
     end
+
   	scenario 'can be reached successfully' do
   		expect(page.status_code).to eq(200)
   	end
+
   	scenario 'has a title of Posts' do
   		expect(page).to have_content(/Posts/)
   	end
+
     scenario 'has a list of posts' do
       post1 = FactoryGirl.build_stubbed(:post)
       post2 = FactoryGirl.build_stubbed(:second_post)
       visit posts_path
       expect(page).to have_content(/Rationale|content/)
     end
+
+    scenario 'has a scope so that only post creator can see thier posts' do
+      post1 = Post.create(date: Date.today, rationale: "abcd", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "abcd", user_id: @user.id)
+
+      other_user = User.create(first_name: "Non", last_name: 'Authorized', 
+                email: "nonauth@gmail.com", password: 'hogehoge', password_confirmation: 'hogehoge' )   
+      post_from_other_user = Post.create(date: Date.today, rationale: "This post shouldn't be seen", user_id: other_user.id)
+
+      visit posts_path
+      expect(page).to_not have_content(/This post shouldn't be seen/)
+    end
+
   end
 
   feature 'new' do
@@ -36,6 +52,7 @@ feature 'navigate' do
   feature 'delete' do
     scenario 'can be deleted' do
       @post = FactoryGirl.create(:post)
+      @post.update(user_id: @user.id)
       visit posts_path
 
       click_link ("delete_post_#{@post.id}_from_index")
